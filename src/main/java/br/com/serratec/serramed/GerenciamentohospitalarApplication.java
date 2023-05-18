99419d04937a776cf5a1c576e4a8c46b8528751c
Nicolas <nicolasjandre@live.com>
2023-05-18 16:59:54 -0300

    :tada: first commit

---
+++ src/main/java/br/com/serratec/serramed/GerenciamentohospitalarApplication.java
@@ -0,0 +1,13 @@
+package br.com.serratec.serramed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentohospitalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentohospitalarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serratec/gerenciamentohospitalar/GerenciamentohospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serratec.gerenciamentohospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentohospitalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
