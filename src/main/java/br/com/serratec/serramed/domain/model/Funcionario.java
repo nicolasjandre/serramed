955bd02d025a434f75ed90b1a371ecc7b44b5134
Nicolas <nicolasjandre@live.com>
2023-05-18 18:55:45 -0300

    :rocket: models finished

---
+++ src/main/java/br/com/serratec/serramed/domain/model/Funcionario.java
@@ -0,0 +1,31 @@
+package br.com.serratec.serramed.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "funcionario_id")
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "departamento_id")
+    private Departamento departamento;
+}
